a6a9bb18f774293758e6d903fc62c1dca18d340f
RaincloudDev <109554891+RaincloudDev@users.noreply.github.com>
2022-11-15 13:36:40 -0800

    Create madlibs.py
    madlib project Team Treehouse

---
+++ madlibs.py
@@ -0,0 +1,18 @@
+# MadLibs - Practice Input and Output
+# raincloudDev 2022 ayo bingBong
+# Template:
+#    I enjoy practice! I find it helps me to __(verb)__ better.
+#    Without practice, my __(noun)__ would probably not even work.
+#    My code is getting more __(adjective)__ every single day!
+
+
+# TODO: Prompt the user for parts of speech and store them in variables
+verb=input('Name a verb! ')
+noun=input('Name a noun! ')
+adj=input('Name an adjective! ')
+
+
+
+# TODO: Output the template to the screen with the blanks filled out with what the user stated
+
+print('I enjoy practice! I find it helps me to', verb, 'better. Without practice, my', noun, 'would probably not even work. My code is getting more', adj, 'every single day!')
